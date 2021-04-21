8b526fa32f2f9b94385e416124129242be459c02
Mellified <81110968+Mellified@users.noreply.github.com>
2021-04-21 14:56:10 -0500

    Add files via upload

--- MiSTer_SAM/MiSTer_SAM_off.sh
+++ MiSTer_SAM/MiSTer_SAM_off.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 /etc/init.d/S93mistersam stop
+
 mount | grep -q "on / .*[(,]ro[,$]" && RO_ROOT="true"
 [ "$RO_ROOT" == "true" ] && mount / -o remount,rw
-mv /etc/init.d/S93mistersam /etc/init.d/_S93mistersam > /dev/null 2>&1
+rm -f /etc/init.d/S93mistersam > /dev/null 2>&1
 sync
 [ "$RO_ROOT" == "true" ] && mount / -o remount,ro
+sync
 
 echo "MiSTer SAM (Super Attract Mode)"
 echo "is off and inactive at startup."
-sync
+
 exit 0
