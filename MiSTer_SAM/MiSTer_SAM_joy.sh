c201db8eae59425a9dc90d9a05a1565c19c43858
Mellified <81110968+Mellified@users.noreply.github.com>
2021-04-24 00:42:02 -0500

    Add files via upload

--- MiSTer_SAM/MiSTer_SAM_joy.sh
+++ MiSTer_SAM/MiSTer_SAM_joy.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 while true; do
-	if [[ $(xxd -l 128 -c 8 ${1} | awk '{ print $4 }' | grep 0100) == "0100" ]]\ # Buttons
-	|| [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "0180 8201") ]\ # Dpad up
-	|| [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "ff7f 8201") ]\ # Dpad down
-	|| [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "ff7f 8200") ]\ # Dpad right
-	|| [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "0180 8200") ]; then # Dpad left
+	if [[ $(xxd -l 128 -c 8 ${1} | awk '{ print $4 }' | grep 0100) == "0100" ]] || [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "0180 8201") ] || [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "ff7f 8201") ] || [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "ff7f 8200") ] || [ $(xxd -l 144 -c 8 ${1} | awk '{ print $4, $5}' | grep "0180 8200") ]; then
 		echo "Button pushed" >| /tmp/.SAM_Joy_Activity
 	fi
 	sleep 0.2 
--- MiSTer_SAM/MiSTer_SAM_joy_change.sh
+++ MiSTer_SAM/MiSTer_SAM_joy_change.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-inotifywait --monitor -e create -e moved_to -e close_write /dev/input/ | while read path action file; do echo "change" > /tmp/.SAM_Joy_Change; done
+inotifywait --monitor -e create -e moved_to -e close_write /dev/input/ | while read path action file; do echo "Device change" >> /tmp/.SAM_Joy_Change; done
