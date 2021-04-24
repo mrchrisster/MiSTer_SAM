c0a784787265d968ac77fc1ef2c482197fdf8e9a
Mellified <81110968+Mellified@users.noreply.github.com>
2021-04-24 14:17:14 -0500

    Add files via upload

--- MiSTer_SAM_on.sh
+++ MiSTer_SAM_on.sh
@@ -25,8 +25,39 @@
 # pocomane, kaloun34, RetroDriven, woelper, LamerDeluxe
 
 
-#======== DEFAULT VARIABLES ========
+# ======== INI VARIABLES ========
+# Change these in the INI file
+samtimeout=300
+gametimer=120
+menuonly="Yes"
+listenmouse="Yes"
+listenkeyboard="Yes"
+listenjoy="Yes"
+corelist="arcade,gba,genesis,megacd,neogeo,nes,snes,tgfx16,tgfx16cd"
+arcadepath="/media/fat/_arcade"
+gbapath="/media/fat/games/GBA"
+genesispath="/media/fat/games/Genesis"
+megacdpath="/media/fat/games/MegaCD"
+neogeopath="/media/fat/games/NeoGeo"
+nespath="/media/fat/games/NES"
+snespath="/media/fat/games/SNES"
+tgfx16path="/media/fat/games/TGFX16"
+tgfx16cdpath="/media/fat/games/TGFX16-CD"
+usezip="Yes"
+disablebootrom="Yes"
+mrapath="/media/fat/_Arcade"
+orientation=All
+mbcpath="/media/fat/Scripts/.MiSTer_SAM/mbc"
+partunpath="/media/fat/Scripts/.MiSTer_SAM/partun"
 mrsampath="/media/fat/Scripts/.MiSTer_SAM"
+misterpath="/media/fat"
+mrapathvert="/media/fat/_Arcade/_Organized/_6 Rotation/_Vertical CW 90 Deg" 
+mrapathhoriz="/media/fat/_Arcade/_Organized/_6 Rotation/_Horizontal"
+
+# ======== DEBUG VARIABLES ========
+startupsleep="Yes"
+samquiet="Yes"
+
 
 #======== BASIC FUNCTIONS ========
 there_can_be_only_one() # there_can_be_only_one PID Process
@@ -227,7 +258,6 @@ config_init()
 #======== DEPENDENCIES ========
 echo "Turning MiSTer SAM on"
 # Read INI
-basepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 if [ -f "${misterpath}/Scripts/MiSTer_SAM.ini" ]; then
 	. "${misterpath}/Scripts/MiSTer_SAM.ini"
 	IFS=$'\n'
