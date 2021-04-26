5036455b24a6c57a2373ad6d0768a740d9470667
mrchrisster <60834906+mrchrisster@users.noreply.github.com>
2021-04-25 17:43:41 -0700

    Update MiSTer_SAM_joy.sh

--- MiSTer_SAM/MiSTer_SAM_joy.sh
+++ MiSTer_SAM/MiSTer_SAM_joy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-	if [[ $(xxd -l 144 -c 8 /dev/input/js0 | awk '{ print $4,$5 }' | grep 'ff7f\|0100\|0180') ]]; then
+	if [[ $(xxd -l 144 -c 8 ${1} | awk '{ print $4,$5 }' | grep 'ff7f\|0100\|0180') ]]; then
 		echo "Button pushed" >| /tmp/.SAM_Joy_Activity
 	fi
 	sleep 0.2 
