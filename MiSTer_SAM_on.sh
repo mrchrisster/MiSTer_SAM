a606caac9c0fa7810a3e5151eaa7e8d9adaeaf4c
Mellified <81110968+Mellified@users.noreply.github.com>
2021-04-26 22:01:31 -0500

    Add files via upload

--- MiSTer_SAM_on.sh
+++ MiSTer_SAM_on.sh
@@ -27,27 +27,13 @@
 
 # ======== INI VARIABLES ========
 # Change these in the INI file
-samtimeout=60
+samtimeout=300
 gametimer=120
 menuonly="Yes"
-corelist="arcade,gba,genesis,megacd,neogeo,nes,snes,tgfx16,tgfx16cd"
-usezip="Yes"
-disablebootrom="Yes"
-mrapath="/media/fat/_Arcade"
-orientation=All
-mraexclude="
-Example Bad Game.mra
-Another Bad Game.mra
-"
 listenmouse="Yes"
 listenkeyboard="Yes"
 listenjoy="Yes"
-mbcpath="/media/fat/Scripts/.MiSTer_SAM/mbc"
-partunpath="/media/fat/Scripts/.MiSTer_SAM/partun"
-mrsampath="/media/fat/Scripts/.MiSTer_SAM"
-misterpath="/media/fat"
-mrapathvert="/media/fat/_Arcade/_Organized/_6 Rotation/_Vertical CW 90 Deg" 
-mrapathhoriz="/media/fat/_Arcade/_Organized/_6 Rotation/_Horizontal"
+corelist="arcade,gba,genesis,megacd,neogeo,nes,snes,tgfx16,tgfx16cd"
 arcadepath="/media/fat/_arcade"
 gbapath="/media/fat/games/GBA"
 genesispath="/media/fat/games/Genesis"
@@ -57,15 +43,20 @@ nespath="/media/fat/games/NES"
 snespath="/media/fat/games/SNES"
 tgfx16path="/media/fat/games/TGFX16"
 tgfx16cdpath="/media/fat/games/TGFX16-CD"
+usezip="Yes"
+disablebootrom="Yes"
+mrapath="/media/fat/_Arcade"
+orientation=All
+mbcpath="/media/fat/Scripts/.MiSTer_SAM/mbc"
+partunpath="/media/fat/Scripts/.MiSTer_SAM/partun"
+mrsampath="/media/fat/Scripts/.MiSTer_SAM"
+misterpath="/media/fat"
+mrapathvert="/media/fat/_Arcade/_Organized/_6 Rotation/_Vertical CW 90 Deg" 
+mrapathhoriz="/media/fat/_Arcade/_Organized/_6 Rotation/_Horizontal"
 
-#======== DEBUG VARIABLES ========
-samquiet="Yes"
-
-# ======== INTERNAL VARIABLES ========
-branch="main"
-partunurl="releases/download/0.1.5/partun_armv7"
-mbcurl="blob/master/mbc_v02"
+# ======== DEBUG VARIABLES ========
 startupsleep="Yes"
+samquiet="Yes"
 
 
 #======== BASIC FUNCTIONS ========
@@ -134,7 +125,7 @@ get_samon() #get_samon process_name
 		cp -f "/tmp/MiSTer_SAM_on.sh" "/media/fat/Scripts/MiSTer_SAM_on.sh"
 	else
 		echo "Downloading MiSTer SAM on"
-		curl_download "/tmp/MiSTer_SAM_on.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM_on.sh?raw=true"
+		curl_download "/tmp/MiSTer_SAM_on.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM_on.sh?raw=true"
 		chmod +x "/tmp/MiSTer_SAM_on.sh"
 	fi
 }
@@ -146,7 +137,7 @@ get_mbc()
 	echo "Created for MiSTer by pocomane"
 	echo "${REPOSITORY_URL}"
 	echo ""
-	curl_download "/tmp/mbc" "${REPOSITORY_URL}/${mbcurl}?raw=true"
+	curl_download "/tmp/mbc" "${REPOSITORY_URL}/blob/master/mbc_v02?raw=true"
 	mv -f "/tmp/mbc" "${mrsampath}/mbc"
 }
 
@@ -157,7 +148,7 @@ get_partun()
 	echo "Created for MiSTer by woelper - who is allegedly not a spider"
 	echo "${REPOSITORY_URL}"
 	echo ""
-	curl_download "/tmp/partun" "${REPOSITORY_URL}/${partunurl}"
+	curl_download "/tmp/partun" "${REPOSITORY_URL}/releases/download/0.1.5/partun_armv7"
 	mv -f "/tmp/partun" "${mrsampath}/partun"
 }
 
@@ -165,7 +156,7 @@ get_sam()
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer Super Attract Mode"
-	curl_download "/tmp/MiSTer_SAM.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM.sh?raw=true"
+	curl_download "/tmp/MiSTer_SAM.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM.sh?raw=true"
 	mv -f "/tmp/MiSTer_SAM.sh" "${mrsampath}/MiSTer_SAM.sh"
 }
 
@@ -173,7 +164,7 @@ get_samoff()
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer SAM off"
-	curl_download "/tmp/MiSTer_SAM_off.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM_off.sh?raw=true"
+	curl_download "/tmp/MiSTer_SAM_off.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM_off.sh?raw=true"
 	mv -f "/tmp/MiSTer_SAM_off.sh" "/media/fat/Scripts/MiSTer_SAM_off.sh"
 }
 
@@ -181,7 +172,7 @@ get_samnow() # No relation to shamwow
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer SAM now"
-	curl_download "/tmp/MiSTer_SAM_now.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM_now.sh?raw=true"
+	curl_download "/tmp/MiSTer_SAM_now.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM_now.sh?raw=true"
 	mv -f "/tmp/MiSTer_SAM_now.sh" "/media/fat/Scripts/MiSTer_SAM_now.sh"
 }
 
@@ -190,7 +181,7 @@ get_ini()
 	if [ ! -f "/media/fat/Scripts/MiSTer_SAM.ini" ]; then
 		REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 		echo "Downloading MiSTer SAM INI"
-		curl_download "/media/fat/Scripts/MiSTer_SAM.ini" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM.ini?raw=true"
+		curl_download "/media/fat/Scripts/MiSTer_SAM.ini" "${REPOSITORY_URL}/blob/main/MiSTer_SAM.ini?raw=true"
 	else
 		echo "SKIPPED MiSTer SAM INI - already exists!"
 	fi
@@ -200,7 +191,7 @@ get_init()
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer SAM daemon"
-	curl_download "/tmp/MiSTer_SAM_init" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM_init?raw=true"
+	curl_download "/tmp/MiSTer_SAM_init" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM_init?raw=true"
 	mv -f "/tmp/MiSTer_SAM_init" "${mrsampath}/MiSTer_SAM_init"
 }
 
@@ -208,7 +199,7 @@ get_joy()
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer SAM controller helper"
-	curl_download "/tmp/MiSTer_SAM_joy.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM_joy.sh?raw=true"
+	curl_download "/tmp/MiSTer_SAM_joy.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM_joy.sh?raw=true"
 	mv -f "/tmp/MiSTer_SAM_joy.sh" "${mrsampath}/MiSTer_SAM_joy.sh"
 }
 
@@ -216,7 +207,7 @@ get_joy_change()
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer SAM controller hotplug monitor"
-	curl_download "/tmp/MiSTer_SAM_joy_change.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM_joy_change.sh?raw=true"
+	curl_download "/tmp/MiSTer_SAM_joy_change.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM_joy_change.sh?raw=true"
 	mv -f "/tmp/MiSTer_SAM_joy_change.sh" "${mrsampath}/MiSTer_SAM_joy_change.sh"
 }
 
@@ -224,7 +215,7 @@ get_keyboard()
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer SAM keyboard helper"
-	curl_download "/tmp/MiSTer_SAM_keyboard.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM_keyboard.sh?raw=true"
+	curl_download "/tmp/MiSTer_SAM_keyboard.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM_keyboard.sh?raw=true"
 	mv -f "/tmp/MiSTer_SAM_keyboard.sh" "${mrsampath}/MiSTer_SAM_keyboard.sh"
 }
 
@@ -232,7 +223,7 @@ get_mouse()
 {
 	REPOSITORY_URL="https://github.com/mrchrisster/MiSTer_SAM"
 	echo "Updating MiSTer SAM mouse helper"
-	curl_download "/tmp/MiSTer_SAM_mouse.sh" "${REPOSITORY_URL}/blob/${branch}/MiSTer_SAM/MiSTer_SAM_mouse.sh?raw=true"
+	curl_download "/tmp/MiSTer_SAM_mouse.sh" "${REPOSITORY_URL}/blob/main/MiSTer_SAM/MiSTer_SAM_mouse.sh?raw=true"
 	mv -f "/tmp/MiSTer_SAM_mouse.sh" "${mrsampath}/MiSTer_SAM_mouse.sh"
 }
 
@@ -276,47 +267,6 @@ fi
 for var in mrsampath; do
 	declare -g ${var}="${!var%/}"
 done
-
-
-if [ "${samquiet,,}" == "no" ]; then
-	echo "mbcurl: ${mbcurl}"
-	echo "samtimeout: ${samtimeout}"
-	echo "gametimer: ${gametimer}"
-	echo "menuonly: ${menuonly}"
-	echo "corelist: ${corelist}"
-	echo "usezip: ${usezip}"
-	echo "disablebootrom: ${disablebootrom}"
-	echo "mrapath: ${mrapath}"
-	echo "orientation: ${orientation}"
-	echo "mraexclude: ${mraexclude}"
-	echo "listenmouse: ${listenmouse}"
-	echo "listenkeyboard: ${listenmouse}"
-	echo "listenjoy: ${listenjoy}"
-	echo "mbcpath: ${mbcpath}"
-	echo "partunpath: ${partunpath}"
-	echo "mrsampath: ${mrsampath}"
-	echo "misterpath: ${misterpath}"
-	echo "mrapathvert: ${mrapathvert}"
-	echo "mrapathhoriz: ${mrapathhoriz}"
-	echo "mralist: ${mralist}"
-	echo "saminterrupt: ${saminterrupt}"
-	echo "arcadepath: ${arcadepath}"
-	echo "gbapath: ${gbapath}"
-	echo "genesispath: ${genesispath}"
-	echo "megacdpath: ${megacdpath}"
-	echo "neogeopath: ${neogeopath}"
-	echo "nespath: ${nespath}"
-	echo "snespath: ${snespath}"
-	echo "tgfx16path: ${tgfx16path}"
-	echo "tgfx16cdpath: ${tgfx16cdpath}"
-
-	# Local variables
-	echo "branch: ${branch}"
-	echo "partunurl: ${partunurl}"
-	echo "mbcurl: ${mbcurl}"
-	echo "startupsleep: ${startupsleep}"
-fi	
-
 
 # Ensure the MiSTer SAM data directory exists
 mkdir "${mrsampath}" &>/dev/null
