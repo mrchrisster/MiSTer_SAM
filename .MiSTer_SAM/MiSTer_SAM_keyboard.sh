c93a2489e554557588cc5e28920db14ad930bc50
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2022-03-28 11:48:50 -0700

    Update MiSTer_SAM_keyboard.sh

--- .MiSTer_SAM/MiSTer_SAM_keyboard.sh
+++ .MiSTer_SAM/MiSTer_SAM_keyboard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-	if [[ $(xxd -l 8 -c32 "/dev/${1}" | cut -c1) == "0" ]]; then
+	if [[ $(hexdump -n 8 -ve '1/1 "%.2x"' "/dev/${1}" | cut -c1) == "0" ]]; then
 		echo "Keyboard used" >| /tmp/.SAM_Keyboard_Activity
 	fi
 	sleep 0.2
