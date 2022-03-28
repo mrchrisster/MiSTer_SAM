a92620c31205a4bf3b64162763014e9e97116599
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2022-03-28 11:49:14 -0700

    Update MiSTer_SAM_mouse.sh

--- .MiSTer_SAM/MiSTer_SAM_mouse.sh
+++ .MiSTer_SAM/MiSTer_SAM_mouse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-	if [[ $(xxd -l 8 -c32 /dev/input/mice | cut -c1) == "0" ]]; then
+	if [[ $( hexdump -n 8 -ve '1/1 "%.2x"' /dev/input/mice | cut -c1) == "0" ]]; then
 		echo "Mouse moved" >| /tmp/.SAM_Mouse_Activity
 	fi
 	sleep 0.2
