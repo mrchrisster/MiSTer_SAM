4f9ff8ee079357abb85c7279f1a7517732c055f0
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2022-05-12 15:36:44 -0700

    Delete MiSTer_SAM_keyboard.sh

--- .MiSTer_SAM/MiSTer_SAM_keyboard.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-	if [[ $(hexdump -n 8 -ve '1/1 "%.2x"' "/dev/${1}" | cut -c1) == "0" ]]; then
-		echo "Keyboard used" >| /tmp/.SAM_Keyboard_Activity
-	fi
-	sleep 0.2
-done
