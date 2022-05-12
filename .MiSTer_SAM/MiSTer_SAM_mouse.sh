1b9179739f1bb69c53e94fff6afb034cca7cf626
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2022-05-12 15:36:49 -0700

    Delete MiSTer_SAM_mouse.sh

--- .MiSTer_SAM/MiSTer_SAM_mouse.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-	if [[ $( hexdump -n 8 -ve '1/1 "%.2x"' /dev/input/mice | cut -c1) == "0" ]]; then
-		echo "Mouse moved" >| /tmp/.SAM_Mouse_Activity
-	fi
-	sleep 0.2
-done
