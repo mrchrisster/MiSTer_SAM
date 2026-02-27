0dd870ad9984839d67800a50751c1a7273fa0911
Christoph Helms <60834906+mrchrisster@users.noreply.github.com>
2026-02-26 22:52:44 -0800

    Modify contributors and change timer delay from 9 to 8
    Updated contributors list and adjusted timer delay.

--- MiSTer_SAM_on.sh
+++ MiSTer_SAM_on.sh
@@ -24,7 +24,7 @@
 # Indexing tool: wizzomafizzo
 #
 # Thanks for the contributions and support:
-# pocomane, kaloun34, redsteakraw, RetroDriven, woelper, LamerDeluxe, InquisitiveCoder, syntax_x, Sigismond, theypsilon
+# pocomane, kaloun34, redsteakraw, RetroDriven, woelper, LamerDeluxe, InquisitiveCoder, syntax_x, Sigismond, theypsilon, TBoneSF
 # tty2oled improvements by venice
 
 # TODO implement playcurrentgame for amiga
@@ -4257,7 +4257,7 @@ function sv_ar_cdi_mode() {
 
     # 8. Calculate Game Timer (+10 second buffer)
     # Using awk to handle the float calc and integer addition in one step
-    local timer_delay=9
+    local timer_delay=8
     sv_gametimer=$(du -m "$tmpvideo" | awk -v delay="$timer_delay" '{print int($1 * 7.5) + delay}')
     
     sv_title="${sv_selected%.*}"
